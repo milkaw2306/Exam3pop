4622d45a868ec92879047b0a7d5e9f7eaadc94e7
ANosok <kirill60278@gmail.com>
2026-02-07 14:48:24 +0500

    Добавьте файлы проекта.

---
+++ News.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Common
+{
+    public class News
+    {
+        public string Name { get; set; }
+        public string Tag { get; set; }
+        public DateTime Date { get; set; }
+        public string Src { get; set; }
+
+        public News (string name, string tag, DateTime date, string src)
+        {
+           this.Name = name;
+           this.Tag = tag;
+           this.Date = date;
+           this.Src = src;
+        }   
+    }
+}
